27119c1258323026ca5cbf2f8aaf219287a2acb9
Saurav Dhillon <souravdhillon010@gmail.com>
2025-01-09 23:09:59 -0500

    Updated PhotoListItem.jsx component file to incorporate style

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,11 +4,17 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <li>
-      <img className="photo-list__item" src={props.photo.imageSource} />
-      <img src={props.photo.profile} />
-      <p>{props.photo.username}</p>
-      <p>{props.photo.location.city}, {props.photo.location.country}</p>
+    <li className="photo-list__item">
+      <img className="photo-list__image" src={props.photo.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.photo.profile} />
+        <div className="photo-list__user-info">
+          <p>{props.photo.username}</p>
+          <div className="photo-list__user-location">
+            <p>{props.photo.location.city}, {props.photo.location.country}</p>
+          </div>
+        </div>
+      </div>
     </li>
   );
 };
