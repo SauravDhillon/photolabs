0e302d8109c47dde95f3ae56304112e59c1ad222
Saurav Dhillon <souravdhillon010@gmail.com>
2025-01-09 23:44:44 -0500

    Updated App.jsx file to include three more photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,9 +16,18 @@ const sampleDataForPhotoListItem = {
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photos = [
+    { ...sampleDataForPhotoListItem, id: "1"},
+    { ...sampleDataForPhotoListItem, id: "2"},
+    { ...sampleDataForPhotoListItem, id: "3"},
+  ];
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem} />
+      <ul className="photo-list">
+        {photos.map((photo) => (
+          <PhotoListItem key={photo.id} photo={photo} />
+        ))}
+        </ul>
     </div>
   );
 };
