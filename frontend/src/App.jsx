e8ce395702b310ce029ee5cd7ff406a487460ee6
Saurav Dhillon <souravdhillon010@gmail.com>
2025-01-10 19:19:53 -0500

    Used mock data from photos.js and topics.js file to display on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
 
 
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,29 +2,33 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
   return (
     <nav className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => {
+      {/* {sampleDataForTopicList.map((topic) => {
+        return <TopicListItem key={topic.id} topicTitle={topic.title} />
+      })} */}
+      {topics.map((topic) => {
         return <TopicListItem key={topic.id} topicTitle={topic.title} />
       })}
     </nav>
