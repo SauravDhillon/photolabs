2233e29c19378815b0d22366c1a0ef6c18b17e31
Saurav Dhillon <souravdhillon010@gmail.com>
2025-01-12 01:56:11 -0500

    Added selected photo to modal using selected Photo state

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({ closeModal }) => {
+const PhotoDetailsModal = ({photo, closeModal }) => {
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button"
               onClick={closeModal}> {/*We just pass ref here not call the function since it will close modal as soon as it opens one( during rendering)if we call it */}
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      {photo && <img className="photo-details-modal__image" src={photo.urls.full} />}
     </div>
   )
 };
