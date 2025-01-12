bc8552f1cc30578da79cd10bfe09cd93eb0a8100
Saurav Dhillon <souravdhillon010@gmail.com>
2025-01-12 02:03:18 -0500

    Added selected photo to modal using selected Photo state

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,6 +4,7 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({photo, closeModal }) => {
+  console.log(photo);
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button"
